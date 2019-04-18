45c46682b77f33cddbc49ed2970c1eb8e9964d09
Евгений Конкин <evgeniykonkin@gmail.com>
2019-04-19 01:03:26 +0400

    Initial Commit

---
+++ playtv-tvos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playtv-tvos
+//
+//  Created by Evgeny Konkin on 19.04.2019.
+//  Copyright © 2019 Evgeny Konkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
