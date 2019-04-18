fbf6d74093ebf34529ff3311691d27c4d778c209
Евгений Конкин <evgeniykonkin@gmail.com>
2019-04-19 01:41:38 +0400

    Install RealmSwift Pods

--- playtv-tvos/View Controllers/MainViewController.swift
+++ playtv-tvos/View Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  playtv-tvos
 //
 //  Created by Evgeny Konkin on 19.04.2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
